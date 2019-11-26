9961f92344b6cd1462090869d0161e799723cd24
lavsexpert <lavrov-sergey@yandex.ru>
2019-11-26 16:12:50 +0300

    Добавлено логирование запросов к серверу

--- app/src/main/java/com/example/kotlinapi/GithubApiService.kt
+++ app/src/main/java/com/example/kotlinapi/GithubApiService.kt
@@ -1,28 +1,37 @@
 package com.example.kotlinapi
 
 import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
 interface GithubApiService {
 
     @GET("search/users")
     fun search(@Query("q") query: String,
-               @Query("page") page: Int,
-               @Query("per_page") perPage: Int): Observable<Result>
+               @Query("sort") sort: String,
+               @Query("order") order: String): Observable<Result>
 
     /**
      * Companion object to create the GithubApiService
      */
     companion object Factory {
         fun create(): GithubApiService {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
             val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("https://api.github.com/")
+                .client(client.build())
                 .build()
 
             return retrofit.create(GithubApiService::class.java)
