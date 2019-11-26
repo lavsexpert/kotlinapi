4aab5e558812497c3c4dc296ba87401b13a7d8bc
lavsexpert <lavrov-sergey@yandex.ru>
2019-11-26 16:28:01 +0300

    Сделан макет экрана и обработка нажатия кнопки

--- app/src/main/java/com/example/kotlinapi/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapi/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.kotlinapi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun search(view: View) {
+        val town = editTown.text.toString()
+        val repository = SearchRepositoryProvider.provideSearchRepository()
+        repository.searchUsers(town)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe ({
+                    result ->
+                Log.d("Result", "There are ${result.total_count} developers in ${town}")
+                textResult.text = "There are ${result.total_count} developers in ${town}"
+            }, { error ->
+                textResult.text = error.localizedMessage
+                error.printStackTrace()
+            })
     }
 }
